61ed4dc37391169c7eb8fab6a9ef6bddf85d30d0
mnabialek <gitre@nabialek.org>
2016-01-27 13:46:45 +0100

    CEP-6 Fixed comment for roles field

--- database/migrations/2016_01_25_072105_create_roles_table.php
+++ database/migrations/2016_01_25_072105_create_roles_table.php
@@ -12,9 +12,10 @@ class CreateRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('roles', function(Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique()->string('Unique identifier of role');
+            $table->string('name')->unique()
+                ->comment('Unique identifier of role');
         });
     }
 
