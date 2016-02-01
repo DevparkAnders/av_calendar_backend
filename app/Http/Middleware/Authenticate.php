b2b3b02a84b60352e522d9a92e56d9a3a5131a9b
mnabialek <gitre@nabialek.org>
2016-02-01 07:56:02 +0100

    CEP-32 - token refreshing

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -22,17 +22,31 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        $tokenExpired = false;
+
         try {
             /** @var User $user */
             $user = JWTAuth::parseToken()->authenticate();
         } catch (TokenExpiredException $e) {
-            return ApiResponse::responseError(ErrorCode::AUTH_EXPIRED_TOKEN,
-                400);
+            $tokenExpired = true;
         } catch (\Exception $e) {
             return ApiResponse::responseError(ErrorCode::AUTH_INVALID_TOKEN,
                 401);
         }
-        
+
+        // token was expired, we will try to refresh it
+        if ($tokenExpired) {
+            try {
+                $user = $this->refreshToken($request);
+            } catch (TokenExpiredException $e) {
+                return ApiResponse::responseError(ErrorCode::AUTH_EXPIRED_TOKEN,
+                    400);
+            } catch (\Exception $e) {
+                return ApiResponse::responseError(ErrorCode::AUTH_INVALID_TOKEN,
+                    401);
+            }
+        }
+
         // we allow authenticate only users that are not deleted
         if (!$user || $user->isDeleted()) {
             return ApiResponse::responseError(ErrorCode::AUTH_USER_NOT_FOUND,
@@ -40,5 +54,21 @@ class Authenticate
         }
 
         return $next($request);
+    }
+
+    /**
+     * Refreshes token when it's expired
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return User
+     */
+    protected function refreshToken($request)
+    {
+        $newToken = JWTAuth::refresh();
+        $request->headers->set('JWTRefreshed', '1', true);
+        $request->headers->set('Authorization', 'Bearer ' . $newToken, true);
+
+        return JWTAuth::authenticate();
     }
 }
--- app/Http/Middleware/RefreshToken.php
+++ app/Http/Middleware/RefreshToken.php
@@ -22,18 +22,13 @@ class RefreshToken
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        
-        try {
-            $newToken = JWTAuth::parseToken()->refresh();
-        } catch (TokenExpiredException $e) {
-            return ApiResponse::responseError(ErrorCode::AUTH_EXPIRED_TOKEN,
-                400);
-        } catch (\Exception $e) {
-            return ApiResponse::responseError(ErrorCode::AUTH_INVALID_TOKEN,
-                401);
-        }
 
-        $response->headers->set('Authorization', 'Bearer ' . $newToken);
+        // if we refreshed token in Authenticate middleware, we attach to
+        // response same header that we put in request
+        if ($request->header('JWTRefreshed', null) == 1) {
+            $response->headers->set('Authorization',
+                $request->header('Authorization'));
+        }
 
         return $response;
     }
