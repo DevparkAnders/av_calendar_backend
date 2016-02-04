bcb3c1c37580f2bf3cc33d275d88268e200ef312
mnabialek <gitre@nabialek.org>
2016-02-04 14:01:25 +0100

    CEP-41 permissions verification base policy

--- config/permissions.php
+++ config/permissions.php
@@ -7,6 +7,8 @@ return [
     // this is list of all available permissions in system
     'all' => [
         'roles.index',
+        'users.index',
+        'users.store',
     ],
 
     // this is assignment of permissions to user roles
@@ -16,11 +18,11 @@ return [
         ],
 
         RoleType::DEALER => [
-
+            'users.index',
         ],
 
         RoleType::DEVELOPER => [
-
+            'users.index',
         ],
 
         RoleType::CLIENT => [
