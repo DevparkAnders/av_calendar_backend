079c751a80c6ff08c1fa12f12a29cffc8640bd13
mnabialek <gitre@nabialek.org>
2016-02-19 11:05:57 +0100

    CEP-47 - changes for CalendarAvailability service

--- app/Modules/CalendarAvailability/Contracts/CalendarAvailability.php
+++ app/Modules/CalendarAvailability/Contracts/CalendarAvailability.php
@@ -2,15 +2,19 @@
 
 namespace App\Modules\CalendarAvailability\Contracts;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+
 interface CalendarAvailability
 {
     /**
-     * Find calendar availability for selected period of time
+     * Find objects with calendar availability for selected period of time
      *
-     * @param string $dateFrom
-     * @param int $limit
+     * @param Carbon $startDate
+     * @param Carbon $endDate
      *
-     * @return array
+     * @return Collection
+     *
      */
-    public function find($dateFrom, $limit);
+    public function find(Carbon $startDate, Carbon $endDate);
 }
